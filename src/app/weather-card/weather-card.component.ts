503443ff8aea0a1583cbf2ffbb609a973338953f
Bartek <bartobloko@gmail.com>
2024-08-02 20:06:32 +0200

    added some icons and weather card info

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,12 +1,14 @@
 import { Component, HostListener } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { LocationService } from '../shared/services/location-service.service';
 import { MatButtonModule } from '@angular/material/button';
+import { log } from 'console';
 
 @Component({
   selector: 'app-weather-card',
   standalone: true,
-  imports: [MatCardModule,MatButtonModule],
+  imports: [MatCardModule,MatButtonModule,MatTooltipModule],
   templateUrl: './weather-card.component.html',
   styleUrl: './weather-card.component.scss'
 })
@@ -17,6 +19,9 @@ export class WeatherCardComponent {
   ){}
   
   private isAnimating = false;
+  indexofHoulyData:number = 0;
+  browserDate = new Date().toUTCString();
+  hourlyData: any
 
   ngOnInit() {
     this.getWeatherData()
@@ -26,14 +31,55 @@ export class WeatherCardComponent {
     this.locationService.location$.subscribe(location => {
       if (location){
         this.locationService.getWeatherData(location).subscribe({ 
-          next: (value) => {
-            console.log(value)
+          next: (res: any) => {
+
+            const { time, temperature_2m, apparent_temperature, cloud_cover, precipitation_probability, relative_humidity_2m, surface_pressure, uv_index, visibility, weather_code } = res.hourly;
+    
+            this.hourlyData = time.map((timestamp: string, index: number) => ({
+                time: timestamp,
+                hour: timestamp,
+                date: timestamp,
+                temperature: temperature_2m[index],
+                apparent_temperature: apparent_temperature[index],
+                cloud_cover: cloud_cover[index],
+                precipitation_probability: precipitation_probability[index],
+                relative_humidity_2m: relative_humidity_2m[index],
+                surface_pressure: surface_pressure[index],
+                uv_index: uv_index[index],
+                visibility: visibility[index],
+                weather_code: weather_code[index]
+            }));
+            this.findCurrentTimeIndex(res)
+            console.log(res,'res')
+            console.log(this.hourlyData[this.indexofHoulyData],'hourlyData');
+            
           },
           error: () => {
           }
         }) 
       }
     })
+  }
+
+  findCurrentTimeIndex(response:any) {
+
+    const { time } = response.hourly;
+    const offsetSeconds = response.utc_offset_seconds;
+    const offsetMilliseconds = offsetSeconds * 1000;
+    
+    const currentUtcDate = new Date();
+    
+    const targetTimezoneDate = new Date(currentUtcDate.getTime() + offsetMilliseconds);
+    
+    const targetTimezoneDateRounded = new Date(Math.round(targetTimezoneDate.getTime() / 3600000) * 3600000);
+    
+    const roundedTimeISO = targetTimezoneDateRounded.toISOString().slice(0, 13) + ":00";
+    
+    console.log("Current UTC Date:", currentUtcDate.toISOString());
+    console.log("Target Timezone Date:", targetTimezoneDate.toISOString());
+    console.log("Rounded Time:", roundedTimeISO);
+
+    this.indexofHoulyData =  time.findIndex((t: string) => t === roundedTimeISO);
   }
 
   @HostListener('document:mousemove', ['$event'])
